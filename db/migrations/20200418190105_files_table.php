2727894c5c1b4f2e80dc980bf004e7c947e17d16
Alexander <ru.karakulin@gmail.com>
2020-04-19 21:11:14 +0500

    Added files table and migrate container

---
+++ db/migrations/20200418190105_files_table.php
@@ -0,0 +1,21 @@
+<?php
+
+use Phinx\Migration\AbstractMigration;
+
+class FilesTable extends AbstractMigration
+{
+    /**
+     * Change Method.
+     *
+     */
+    public function change()
+    {
+        $this->table('files')
+            ->addColumn('name', 'string')
+            ->addColumn('path', 'string')
+            ->addColumn('real_location', 'string')
+            ->addColumn('deleted', 'boolean', [ 'default' => false ])
+            ->addTimestamps()
+            ->create();
+    }
+}
